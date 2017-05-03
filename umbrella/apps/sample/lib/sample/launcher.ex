564e6ddc80d50cab11e7884f604c1526951a48db
BenjaminPLee <leebenjp@gmail.com>
2017-05-03 14:43:32 +0000

    made initial launch not happen on app start

--- umbrella/apps/sample/lib/sample/launcher.ex
+++ umbrella/apps/sample/lib/sample/launcher.ex
@@ -2,13 +2,18 @@ defmodule Sample.Launcher do
   use GenServer
   require Logger
 
+  @name Sample.Launcher
+
   def start_link do
-    GenServer.start_link(__MODULE__, :ok, [])
+    GenServer.start_link(__MODULE__, :ok, name: @name)
+  end
+
+  def launch() do
+    GenServer.cast(@name, :launch)
   end
 
   def init(state) do
     Logger.info "Sample Agent Launcher Starting"
-    GenServer.cast(self(), :launch)
     {:ok, state}
   end
 
